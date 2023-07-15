ecf0cfb78e8b4938ba6d396e4ffbd4a50cd42603
Javid izadi <javid.izadi1384@gmail.com>
2023-07-15 10:26:30 +0330

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM archlinux:latest
+RUN pacman -Suy --noconfirm --needed git base-devel bc cpio libelf pahole xmlto github-cli
+RUN useradd -m linux-cachyos_builder
+USER linux-cachyos_builder
+RUN mkdir /home/linux-cachyos_builder/.config
+#COPY modprobed.db /home/linux-cachyos_builder/.config/modprobed.db
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+commit_hash="234a03d7790061aff8aed5a12850899ea6f780a7"
+repo=`printenv REPO`
+cd /home/linux-cachyos_builder
+git clone https://aur.archlinux.org/linux-cachyos.git &&
+cd linux-cachyos
+echo "Checkout specified commit..."
+git checkout $commit_hash &&
+echo "Compiling kernel..."
+#env MAKEFLAGS="-s -j$(nproc)" _localmodcfg=y _subarch=22 makepkg --skippgpcheck &&
+env _processor_opt="sandybridge" _disable_debug=y _NUMAdisable=y _nr_cpus=4 _cc_harder=y _use_auto_optimization='' makepkg
+echo "Logining in to GitHub..."
+printenv GITHUB_KEY | gh auth login --with-token
+#version=`git log --format=%B -n 1 $commit_hash | awk -F '-' 'NR==1{print "v"$1}'`
+version='6.4.3-1'
+gh release view "$version" --repo "$repo"
+tag_exists=$?
+if test $tag_exists -eq 0; then
+    echo "Tag already exists!"
+    echo "Removing previous release..."
+    gh release delete "$version" -y --cleanup-tag --repo "$repo"
+else
+    echo "Tag does not exist!"
+fi
+echo "Releasing $version binaries into $repo"
+gh release create "$version" ./*.pkg.tar.zst --repo "$repo" &&
+echo "Released!"
+echo "Loging out from Github..."
+gh auth logout -h github.com
